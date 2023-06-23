53cd529a14525aa3cf267a5ebad51c380e8d5c8f
Thao Pham <thao.phamduy@asnet.com.vn>
2023-06-23 16:11:38 +0700

    fix: update IBook

--- src/interface/book.ts
+++ src/interface/book.ts
@@ -1,10 +1,10 @@
 export interface IBook {
   id?: string;
-  title: string;
-  categoryName: string;
-  description: string;
-  image: string;
-  author: string;
-  published: string;
-  publishers: string;
+  title?: string;
+  categoryName?: string;
+  description?: string;
+  image?: string;
+  author?: string;
+  published?: string;
+  publishers?: string;
 }
